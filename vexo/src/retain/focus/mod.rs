c8b4954461e5eee4dc08a438b5cad6081ae5152a
fourier.wang <fourier.wang@temu.com>
2026-05-20 17:51:10 +0800

    feat: add FocusScopeData, UnfocusDisposition, and TraversalPolicy

--- vexo/src/retain/focus/mod.rs
+++ vexo/src/retain/focus/mod.rs
@@ -1,5 +1,9 @@
 mod key;
 mod node;
+mod scope;
+mod traversal;
 
 pub use key::FocusNodeKey;
 pub use node::FocusNodeData;
+pub use scope::{FocusScopeData, UnfocusDisposition};
+pub use traversal::TraversalPolicy;
