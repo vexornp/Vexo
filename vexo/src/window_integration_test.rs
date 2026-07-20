8ac82f95de14da73024e482df95fdf7ce0e5793f
peiyan.wpy <peiyan.wpy@gmail.com>
2026-07-20 23:53:47 +0800

    refactor: migrate vexo/src/window_integration_test.rs to MultiChild/WithLayout/DecoratedBox model

--- vexo/src/window_integration_test.rs
+++ vexo/src/window_integration_test.rs
@@ -6,7 +6,7 @@ mod tests {
     use crate::core::Color;
     use crate::layout::Layout;
     use crate::widgets::{DecoratedBox, WithLayout};
-    use crate::{Flex, Text, Widget};
+    use crate::{children, MultiChild, Text, Widget};
 
     #[test]
     fn test_retain_view_returns_widget_tree() {
@@ -36,10 +36,10 @@ mod tests {
         let dc2 = DecoratedBox::new(WithLayout::new(Text::new("Second"), Layout::default()))
             .background(Color::GREEN);
 
-        let col = Flex::column().push(dc1).push(dc2);
+        let col = MultiChild::new(children![dc1, dc2], Layout::column());
 
         // Verify column has children
         let col_any = col.as_any();
-        assert!(col_any.downcast_ref::<Flex>().is_some());
+        assert!(col_any.downcast_ref::<MultiChild>().is_some());
     }
 }
